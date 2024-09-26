21df13554eff5f42484c14e0e94ef47a91cb94e8
Akshay <akshayshinde61493@gmail.com>
2024-09-26 16:18:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ border: "2px solid black" }}>
+      <div
+        style={{
+          display: "flex",
+          backgroundColor: "blue",
+          color: "white",
+          justifyContent: "space-between",
+          margin: "20px",
+        }}
+      >
+        <h6 style={{ margin: "20px" }}>Inventory Management System</h6>
+        <h6 style={{ margin: "20px" }}> Home</h6>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <form>
+          <input type="checkbox"></input>
+          <label>Item</label>
+          <input type="checkbox"></input>
+          <label>Supplier</label>
+        </form>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <h2>Item Details</h2>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignContent: "center",
+            justifyContent: "center",
+            backgroundColor: "lightblue",
+            width: "70%",
+            height: "300px",
+            borderRadius: "20px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <form>
+            <label>Item Name</label>
+            <br></br>
+            <input type="text"></input>
+            <br></br>
+            <label>Quntity</label>
+            <br></br>
+            <input type="number"></input>
+            <br></br>
+            <label>Unit Price</label>
+            <br></br>
+            <input type="number"></input>
+            <br></br>
+            <label>Date Of Submission</label>
+            <br></br>
+            <input type="Date"></input>
+            <br></br>
+          </form>
+        </div>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <h2>Suplier Detail</h2>
+
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+      <div
+          style={{
+            display: "flex",
+            alignContent: "center",
+            justifyContent: "center",
+            backgroundColor: "lightblue",
+            width: "70%",
+            height: "300px",
+            borderRadius: "20px",
+          }}
+        >
+          <form>
+            <label>Suplier Name</label>
+            <br></br>
+            <input type="text"></input>
+            <br></br>
+            <label>Company Name</label>
+            <br></br>
+            <input type="text"></input>
+            <br></br>
+            <label>Contry</label>
+            <br></br>
+            <input type="text" >
+            
+            </input>
+            <br></br>
+            <label>State</label>
+            <br></br>
+            <input type="text"></input>
+            <br></br>
+            <label>City</label>
+            <br></br>
+            <input type="text"></input>
+            <br></br>
+            <label>Email Adress</label>
+            <br></br>
+            <input type="text"></input>
+            <br></br>
+          </form>
+        </div>
+        </div>
+      
+      
+
     </div>
   );
 }
